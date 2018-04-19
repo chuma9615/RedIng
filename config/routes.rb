f7b10a3b2e834750e9a3b5496eeee6885d26a3e6
Francisco Larach <fjlarach@uc.cl>
2018-04-19 16:09:37 -0300

    Welcome, LogIn, SignIn

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,18 @@
 
 Rails.application.routes.draw do
+
   get 'welcome/index'
-
   resources :articles
-
   root 'welcome#index'
+
+  get 'signup'  => 'users#new'
+  resources :users
+
+  get '/login'  => 'sessions#new'
+  resources :sessions
+
+  post 'login' => 'sessions#create'
+
+  delete 'logout' => 'sessions#destroy'
+  
 end
