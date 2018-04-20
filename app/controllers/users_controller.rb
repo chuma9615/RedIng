5dc793058e114dbc7058db2f09bf62753cd35c9b
Francisco Larach <fjlarach@uc.cl>
2018-04-20 13:06:37 -0300

    Avance

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,18 +1,31 @@
 class UsersController < ApplicationController
+
+
   def new
     @user = User.new
   end
+
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+
   def create
     @user = User.new(user_params)
     if @user.save
       session[:user_id] = @user.id
-      redirect_to '/'
+      redirect_to user_path(@user)
     else
       redirect_to '/signup'
     end
   end
+
+
   private
   def user_params
-    params.require(:user).permit(:first_name, :last_name, :email, :password)
+    params.require(:user).permit(:first_name, :last_name, :email, :password, :born_year)
   end
+
+
 end
