a93015cae0d8c2d96d970b573e46a00b621361a9
Domingo Jose Ramirez Calvo <chuma@Domingos-MacBook-Pro.local>
2018-04-21 10:31:39 -0300

    foro

---
+++ app/controllers/forums_controller.rb
@@ -0,0 +1,29 @@
+class ForumsController < ApplicationController
+  def new
+    @forum = Forum.new
+  end
+
+  def index
+    @forums = Forum.all
+  end
+
+  def show
+    @forum = Forum.find(params[:id])
+  end
+
+  def create
+    @forum = Forum.new(forum_params)
+    respond_to do |format|
+      if @forum.save
+        format.html { redirect_to forum_path(@forum), notice: "Se ha creado con exito el foro"}
+      else
+        format.html {render :new}
+      end
+    end
+  end
+
+  private
+    def forum_params
+      params.require(:forum).permit(:name, :description)
+    end
+end
