27ba9f705ddb0fd5b556ec89b6c1c86765e5c70c
Federico Brunner <fobrunner@uc.cl>
2018-04-30 23:14:08 -0300

    blabla

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -20,9 +20,7 @@ class CommentsController < ApplicationController
 
     respond_to do |format|
       if @comment.save
-        format.html {redirect_to forum_article_comment_path(@forum, @article, @comment)
-           notice: "Comentario creado con éxito" }
-        format.json { render :show, status: :created, location: @comment }
+        format.html {redirect_to forum_article_path(@forum, @article) }
       else
         format.html { render :new }
       end
@@ -38,7 +36,7 @@ class CommentsController < ApplicationController
 
   private
     def comment_params
-      params.require(:comment).permit(:op, :content, :article)
+      params.require(:comment).permit(:op, :content, :article_id)
     end
 
     def set_article_comment
