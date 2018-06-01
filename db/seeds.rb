299f7994b58833e40436435e5def4080a70ecece
Domingo Ramirez <djramirez@uc.cl>
2018-06-01 09:29:17 -0400

    change seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,3 @@
 User.create!([
-  {first_name: "Domigo", last_name: "Ramirez", email: "djramirez@uc.cl", password_digest: "$2a$10$czinRmh1rQAeLVfN20ESxOIiLTsdrbIwoJDPJc6MQ0ObZvKzp0JEa", born_year: "1996", avatar_url: ""}
+  {first_name: "Domingo", last_name: "Ramirez", email: "djramirez@uc.cl", password_digest: "$2a$10$czinRmh1rQAeLVfN20ESxOIiLTsdrbIwoJDPJc6MQ0ObZvKzp0JEa", born_year: "1996", avatar_url: ""}
 ])
