590ae0e7a75985c3cd439ad3b2b11fb7c00693e3
Federico Brunner <fobrunner@uc.cl>
2018-06-21 12:15:24 -0400

    maximo largo para comentarios, foros y articulos

--- app/models/forum.rb
+++ app/models/forum.rb
@@ -8,5 +8,9 @@ class Forum < ApplicationRecord
   #has_and_belongs_to_many :users
   acts_as_followable
   validates :name, :description, presence: true
+  validates :name, length: { maximum: 25,
+    too_long: "%{count} caracteres es lo máximo permitido para el nombre del foro." }
+  validates :description, length: { maximum: 100,
+    too_long: "%{count} caracteres es lo máximo permitido para la descripción del foro." }
 
 end
